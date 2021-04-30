ec1e6d5d063c3b0d48c3f4e602e3a5b4e5abc0d5
Kerry Lang <klang812@gmail.com>
2021-04-30 10:15:15 -0700

    test rootReducer actions

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -21,4 +21,26 @@ describe("rootReducer", () => {
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
+
+  test('Check that ADD_KEG action works for kegListReducer and rootReducer', () => {
+    const action = {
+      type: 'ADD_KEG',
+      name: 'IPA',
+      brand: 'Migration',
+      price: '4.00',
+      alcoholContent: '7.4',
+      quantity: '50',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and rootReducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
 });
