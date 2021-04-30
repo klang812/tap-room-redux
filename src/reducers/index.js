5a05eb3506bfd3146caa31383b518f343812e193
Kerry Lang <klang812@gmail.com>
2021-04-30 10:01:18 -0700

    add combineReducers test to pass

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import formVisibleReducer from './form-visible-reducer';
+import ketListReducer from './keg-list-reducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterKegList: ketListReducer 
+});
+
+export default rootReducer;
