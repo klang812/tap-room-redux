91d40107cf764753e34da1b3e47dde6b6358fa26
Kerry Lang <klang812@gmail.com>
2021-04-30 10:08:59 -0700

    add index smoke tests for reducers

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,10 +1,24 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
+
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       masterKegList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test('Check that initial state of ketListReducer matches root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 });
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import formVisibleReducer from './form-visible-reducer';
 import ketListReducer from './keg-list-reducer';
-import { combineReducers } from 'redux';
+import { combineReducers, createStore } from 'redux';
+
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
